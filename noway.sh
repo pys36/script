d2540dc9a16d853e65f34625a9f629608fb98ac1
Pang YS <devbx@proton.me>
2024-12-14 01:34:34 +0800

    Update noway.sh

--- noway.sh
+++ noway.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 rm -rf .repo/local_manifests/
-git clone https://github.com/pys36/local_manifests.git .repo/local_manifests
 repo init -u https://github.com/ProjectEverest/manifest -b 14 --git-lfs
+git clone https://github.com/pys36/local_manifests.git .repo/local_manifests
 /opt/crave/resync.sh
 . build/envsetup.sh
 export TZ=Asia/Kuching
 export BUILD_USERNAME=pangys
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=1
-lunch lineage_earth-user
+lunch lineage_earth-ap2a-user
 mka everest -j$(nproc --all)
