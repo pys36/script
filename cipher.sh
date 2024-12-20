fc7fbae3c6290957dfb5d5269b5d3c4c00fd7e14
Pang YS <devbx@proton.me>
2024-12-20 13:58:08 +0800

    Create cipher.sh

---
+++ cipher.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -rf .repo/local_manifests/
+git clone -b cipher https://github.com/pys36/local_manifests.git .repo/local_manifests
+/opt/crave/resync.sh
+. build/envsetup.sh
+export TZ=Asia/Kuching
+export BUILD_USERNAME=pangys
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+. build/envsetup.sh
+lunch cipher_earth-ap1a-userdebug
+mka bacon -j$(nproc --all)
