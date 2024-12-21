0bbbef8760dddd501d0e965abfa137d1b97b2bb8
Pang YS <devbx@proton.me>
2024-12-21 22:51:26 +0800

    Update cipher.sh

--- cipher.sh
+++ cipher.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -rf .repo/local_manifests/
+repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr
 git clone -b cipher https://github.com/pys36/local_manifests.git .repo/local_manifests
 /opt/crave/resync.sh
 . build/envsetup.sh
@@ -8,5 +9,5 @@ export BUILD_USERNAME=pangys
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 . build/envsetup.sh
-lunch cipher_earth-ap2a-userdebug
+lunch cipher_earth-ap1a-userdebug
 mka bacon -j$(nproc --all)
